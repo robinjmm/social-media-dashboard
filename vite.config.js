9cbb70a7f6f46b3535782bbfd0d45f32c186c17e
JMMR <103889221+robinjmm@users.noreply.github.com>
2023-03-19 22:30:30 +0800

    Setup project to use Vite as the bundler

---
+++ vite.config.js
@@ -0,0 +1,31 @@
+import { defineConfig } from "vite";
+import { createHtmlPlugin } from "vite-plugin-html";
+import postcssPresetEnv from "postcss-preset-env";
+import purgeCSSPlugin from "@fullhuman/postcss-purgecss";
+import path from "path";
+
+export default defineConfig({
+	resolve: {
+		alias: {
+			"@styles": path.resolve(__dirname, "./scss"),
+			"@fontawesome": path.resolve(__dirname, "./node_modules/@fortawesome/fontawesome-free/css"),
+		},
+	},
+	plugins: [
+		createHtmlPlugin({
+			minify: true,
+		}),
+	],
+	css: {
+		postcss: {
+			plugins: [
+				postcssPresetEnv,
+				purgeCSSPlugin({
+					content: ["./index.html"],
+					variables: true,
+					keyframes: true,
+				}),
+			],
+		},
+	},
+});
