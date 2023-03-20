e2119af5e9017ab3dfd24fc9def72da1f3f6f87a
JMMR <103889221+robinjmm@users.noreply.github.com>
2023-03-20 17:44:22 +0800

    Add GitHub repo link

--- js/main.js
+++ js/main.js
@@ -1,2 +1,6 @@
 // eslint-disable-next-line import/no-unresolved
+import "@fontawesome/fontawesome.min.css";
+// eslint-disable-next-line import/no-unresolved
+import "@fontawesome/brands.min.css";
+// eslint-disable-next-line import/no-unresolved
 import "@styles/styles.scss";
